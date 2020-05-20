17686401ab5bc224a07b6e24849cd96abd818a13
Midnight Coder <62158642+midni8coder@users.noreply.github.com>
2020-05-21 00:06:01 +0530

    Initial upload - Mobile Application

---
+++ www/js/index.js
@@ -0,0 +1,47 @@
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+    onEditClick:function(control,formID, type){
+        
+        if($(control).text().toLowerCase() == "edit"){
+            $(control).removeClass('ui-icon-edit').addClass('ui-icon-check').text('Save');
+            $('[name="name"]','#'+formID).removeAttr('disabled');
+            $('[name="name"]','#'+formID).parent().removeClass("ui-state-disabled");
+        }
+        else
+        {
+            $(control).removeClass('ui-icon-check').addClass('ui-icon-edit').text('Edit');
+            $('[name="name"]','#'+formID).attr('disabled','disabled');
+            $('[name="name"]','#'+formID).parent().addClass("ui-state-disabled");
+        }
+        
+    }
+};
