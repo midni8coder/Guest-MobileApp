dbe0dd77a8d31dc546e83f92f695d023a7c78482
Midnight Coder <62158642+midni8coder@users.noreply.github.com>
2020-05-23 03:04:40 +0530

    added pie and bar charts in home page

--- www/js/index.js
+++ www/js/index.js
@@ -50,14 +50,22 @@ var app = {
         
         if($(control).text().toLowerCase() == "edit"){
             $(control).removeClass('ui-icon-edit').addClass('ui-icon-check').text('Save');
-            $('[name="name"]','#'+formID).removeAttr('disabled');
-            $('[name="name"]','#'+formID).parent().removeClass("ui-state-disabled");
+            // $('[name="name"]','#'+formID).removeAttr('disabled');
+            // $('[name="name"]','#'+formID).parent().removeClass("ui-state-disabled");
+            $.each($('input:visible','#'+formID), function(id,value){
+                $(value).removeAttr('disabled');
+                $(value).parent().removeClass("ui-state-disabled");;
+            })
         }
         else
         {
             $(control).removeClass('ui-icon-check').addClass('ui-icon-edit').text('Edit');
-            $('[name="name"]','#'+formID).attr('disabled','disabled');
-            $('[name="name"]','#'+formID).parent().addClass("ui-state-disabled");
+            // $('[name="name"]','#'+formID).attr('disabled','disabled');
+            // $('[name="name"]','#'+formID).parent().addClass("ui-state-disabled");
+            $.each($('input:visible','#'+formID), function(id,value){
+                $(value).attr('disabled','disabled');
+                $(value).parent().addClass("ui-state-disabled");
+            })
         }
         
     },
@@ -93,5 +101,36 @@ var app = {
     },
     AddNewGuestSubmit:function(){
         alert('submitting')
-    }
+    },
+    renderChart:function() {
+
+        var chart = new CanvasJS.Chart("chartContainer", {
+            theme: "light2", // "light1", "light2", "dark1", "dark2"
+            exportEnabled: true,
+            animationEnabled: true,
+            title: {
+                text: "Desktop Browser Market Share in 2016"
+            },
+            data: [{
+                type: "pie",
+                startAngle: 25,
+                toolTipContent: "<b>{label}</b>: {y}%",
+                showInLegend: "true",
+                legendText: "{label}",
+                indexLabelFontSize: 16,
+                indexLabel: "{label} - {y}%",
+                dataPoints: [
+                    { y: 51.08, label: "Chrome" },
+                    { y: 27.34, label: "Internet Explorer" },
+                    { y: 10.62, label: "Firefox" },
+                    { y: 5.02, label: "Microsoft Edge" },
+                    { y: 4.07, label: "Safari" },
+                    { y: 1.22, label: "Opera" },
+                    { y: 0.44, label: "Others" }
+                ]
+            }]
+        });
+        chart.render();
+        
+        }
 };
