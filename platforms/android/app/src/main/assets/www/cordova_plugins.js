0e6cb38f61062b1be6ad54c58c3fc643c7475748
Midnight Coder <62158642+midni8coder@users.noreply.github.com>
2020-06-14 13:59:33 +0530

    CORS issue

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -391,6 +391,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-media-capture": "1.4.3",
     "cordova-plugin-network-information": "1.3.4",
     "cordova-plugin-statusbar": "2.4.3",
-    "cordova-plugin-vibration": "2.1.6"
+    "cordova-plugin-vibration": "2.1.6",
+    "cordova-plugin-whitelist": "1.3.4"
   };
 });
